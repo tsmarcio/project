7887b35baaf74f0af3d63f119394d0256f7832e0
Marcio Teles Da Silva <91088080+tsmarcio@users.noreply.github.com>
2022-12-01 22:56:29 -0300

    Add files via upload

---
+++ crud.js
@@ -0,0 +1,24 @@
+{
+id: 'uuid', name:
+'João Silva', password:
+'***********',
+username: 'joao.silva',
+
+    id: 'uuid'
+    title: 'Nome do projeto',
+    zip_code: 88010400,
+    cost: 9500
+    done: false,
+    deadline: '2022-09-31T00:00:00.000Z',
+    username: ‘joao.silva’, created_at: '2022-09-
+    26T00:00:00.000Z'
+    updated_at: '2022-09-26T00:00:00.000Z'
+    
+    
+
+}
+
+console.log id 
+
+
+
